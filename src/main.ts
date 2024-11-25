d9eb085b46f04614f283fe06582625b524057754
Soso <soso.beriashvili@redberry.ge>
2024-11-25 16:52:07 +0400

    feat: add logger values to .env example, update main ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingService } from './logging/logging.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
 
   const port = process.env.PORT || 4000;
 
+  app.useLogger(app.get(LoggingService));
   await app.listen(port);
 }
 bootstrap();
